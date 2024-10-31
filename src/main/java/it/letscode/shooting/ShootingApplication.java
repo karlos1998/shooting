d02248fdf592b2379cc52997826ec9f514313115
karlos1998 <fadeusz98@gmail.com>
2024-10-31 01:25:09 +0100

    github workflow

--- src/main/java/it/letscode/shooting/ShootingApplication.java
+++ src/main/java/it/letscode/shooting/ShootingApplication.java
@@ -3,6 +3,9 @@ package it.letscode.shooting;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.letscode.shooting.Question.Question;
+import it.letscode.shooting.Question.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,21 +14,27 @@ import java.io.IOException;
 import java.util.List;
 
 @SpringBootApplication
-public class ShootingApplication {
+public class ShootingApplication implements CommandLineRunner {
+
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShootingApplication.class, args);
 	}
 
-	/**
-	 * questionRepository.deleteAll();
-	 *
-	 * 		File answersFile = new File("src/main/resources/questions.json");
-	 * 		ObjectMapper objectMapper = new ObjectMapper();
-	 * 		List<Question> questions = objectMapper.readValue(answersFile, new TypeReference<List<Question>>() {});
-	 * 		questionRepository.saveAll(questions);
-	 * 		System.out.println("Zapisano " + questions.size() + " rekordów do bazy danych.");
-	 * 	    }
-	 */
-
+	@Override
+	public void run(String... args) throws IOException {
+		for (String arg : args) {
+			if ("--init-db".equals(arg)) {
+				questionRepository.deleteAll();
+				File answersFile = new File("src/main/resources/questions.json");
+				ObjectMapper objectMapper = new ObjectMapper();
+				List<Question> questions = objectMapper.readValue(answersFile, new TypeReference<List<Question>>() {});
+				questionRepository.saveAll(questions);
+				System.out.println("Zapisano " + questions.size() + " rekordów do bazy danych.");
+				System.exit(0);
+			}
+		}
+	}
 }
